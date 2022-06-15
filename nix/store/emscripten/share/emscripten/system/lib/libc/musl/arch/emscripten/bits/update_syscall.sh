af3ef89d562d5d1f8e86482058fb21819e12fa09
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2022-06-15 21:07:43 +0100

    Delete nix directory

--- nix/store/emscripten/share/emscripten/system/lib/libc/musl/arch/emscripten/bits/update_syscall.sh
+++
@@ -1,7 +0,0 @@
-#!/nix/store/bcwyhqvik6jhcklib33v6kykdl4zp9d4-bash-5.1-p8/bin/sh
-# This scripe updates syscall.h based on the contents of syscall.h.in
-# essentially duplicate all the __NR_ marcros with __SYS_ macros.  In upstream
-# must this is don by the top level Makefile.
-
-cp syscall.h.in syscall.h
-sed -n -e s/__NR_/SYS_/p < syscall.h.in >> syscall.h
--- nix/store/emscripten/share/emscripten/tests/third_party/freealut/autogen.sh
+++
@@ -1,2 +0,0 @@
-#!/nix/store/bcwyhqvik6jhcklib33v6kykdl4zp9d4-bash-5.1-p8/bin/sh
-autoreconf --install --force --warnings=all
