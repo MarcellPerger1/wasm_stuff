ddd39aa1299dd214f2cc8166216e756af8aa2fe9
MarcellPerger1 <102254594+MarcellPerger1@users.noreply.github.com>
2022-06-15 19:26:52 +0000

    Remove ccls-cache

--- .ccls-cache/@home@runner@wasmstuff/examle.cpp
+++
@@ -1,9 +0,0 @@
-#include <emscripten.h>
-#include <emscripten/bind.h>
-
-using namespace emscripten;
-int main() {
-        val xhr = val::global("XMLHttpRequest").new_();
-        xhr.call<void>("open", std::string("GET"), std::string("http://url"));
-        return 0;
-}
